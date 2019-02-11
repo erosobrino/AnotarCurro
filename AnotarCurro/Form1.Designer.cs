0f20c8a6cf8262230d2721d2402fa2ae7b84508c
Ero Sobrino <ero.sobrino.dorado@gmail.com>
2019-02-11 12:02:37 +0100

    11/2/19

--- AnotarCurro/Form1.Designer.cs
+++ AnotarCurro/Form1.Designer.cs
@@ -28,13 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btAñadir = new System.Windows.Forms.Button();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btCambiarDni = new System.Windows.Forms.Button();
+            this.txtDni = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtPuerto = new System.Windows.Forms.TextBox();
+            this.btGuardar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btAñadir
+            // 
+            this.btAñadir.Location = new System.Drawing.Point(12, 12);
+            this.btAñadir.Name = "btAñadir";
+            this.btAñadir.Size = new System.Drawing.Size(158, 91);
+            this.btAñadir.TabIndex = 0;
+            this.btAñadir.Text = "Añadir";
+            this.btAñadir.UseVisualStyleBackColor = true;
+            this.btAñadir.Click += new System.EventHandler(this.btAñadir_Click);
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txtInfo.Location = new System.Drawing.Point(0, 109);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.ReadOnly = true;
+            this.txtInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtInfo.Size = new System.Drawing.Size(492, 341);
+            this.txtInfo.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(180, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Dni";
+            // 
+            // btCambiarDni
+            // 
+            this.btCambiarDni.Location = new System.Drawing.Point(359, 9);
+            this.btCambiarDni.Name = "btCambiarDni";
+            this.btCambiarDni.Size = new System.Drawing.Size(121, 44);
+            this.btCambiarDni.TabIndex = 4;
+            this.btCambiarDni.Text = "Cambiar";
+            this.btCambiarDni.UseVisualStyleBackColor = true;
+            this.btCambiarDni.Click += new System.EventHandler(this.btCambiarDni_Click);
+            // 
+            // txtDni
+            // 
+            this.txtDni.Location = new System.Drawing.Point(232, 12);
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(110, 22);
+            this.txtDni.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(180, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "IP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(180, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Puerto";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(232, 47);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(110, 22);
+            this.txtIP.TabIndex = 8;
+            // 
+            // txtPuerto
+            // 
+            this.txtPuerto.Location = new System.Drawing.Point(232, 81);
+            this.txtPuerto.Name = "txtPuerto";
+            this.txtPuerto.Size = new System.Drawing.Size(110, 22);
+            this.txtPuerto.TabIndex = 9;
+            // 
+            // btGuardar
+            // 
+            this.btGuardar.Location = new System.Drawing.Point(359, 59);
+            this.btGuardar.Name = "btGuardar";
+            this.btGuardar.Size = new System.Drawing.Size(121, 44);
+            this.btGuardar.TabIndex = 10;
+            this.btGuardar.Text = "Guardar Datos";
+            this.btGuardar.UseVisualStyleBackColor = true;
+            this.btGuardar.Click += new System.EventHandler(this.btGuardar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(492, 450);
+            this.Controls.Add(this.btGuardar);
+            this.Controls.Add(this.txtPuerto);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDni);
+            this.Controls.Add(this.btCambiarDni);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.btAñadir);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Servidor Curro";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btAñadir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btCambiarDni;
+        private System.Windows.Forms.TextBox txtDni;
+        public System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtPuerto;
+        private System.Windows.Forms.Button btGuardar;
     }
 }
 
--- AnotarCurro/Form1.cs
+++ AnotarCurro/Form1.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +15,224 @@ namespace AnotarCurro
 {
     public partial class Form1 : Form
     {
+        string directory = Environment.GetEnvironmentVariable("homedrive") + "\\" + Environment.GetEnvironmentVariable("homepath") + "\\configCurro.txt";
+        string dni;
+        string IP_SERVER;
+        int port;
+        IPEndPoint ie;
+        Socket server;
+        NetworkStream ns;
+        StreamReader sr;
+        StreamWriter sw;
+        bool valido = true;
+        bool datos = false;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void cerrarConexion()
+        {
+            try
+            {
+                sr.Close();
+                sw.Close();
+                ns.Close();
+                server.Close();
+            }
+            catch { }
+        }
+
+        private void conectar()
+        {
+            try
+            {
+                txtInfo.Clear();
+                ie = new IPEndPoint(IPAddress.Parse(IP_SERVER), port);
+                server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                try
+                {
+                    server.Connect(ie);
+                    ns = new NetworkStream(server);
+                    sr = new StreamReader(ns);
+                    sw = new StreamWriter(ns);
+                    try
+                    {
+                        sw.WriteLine("user " + dni);
+                        sw.Flush();
+                        sr.ReadLine();
+                    }
+                    catch (IOException)
+                    {
+                        txtInfo.AppendText("Sin conexion con el servidor");
+                    }
+                }
+                catch (SocketException ee)
+                {
+                    txtInfo.AppendText(String.Format("Error connection: {0}\nError code: {1}({2})", ee.Message, (SocketError)ee.ErrorCode, ee.ErrorCode));
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                txtInfo.AppendText("Introduce los datos primero");
+            }
+        }
+
+        private void btAñadir_Click(object sender, EventArgs e)
+        {
+            bool acabado = false;
+            conectar();
+            if (datos)
+            {
+                try
+                {
+                    sw.WriteLine("add");
+                    sw.Flush();
+                    while (!acabado)
+                    {
+                        string cadena = sr.ReadLine();
+                        if (cadena != null)
+                        {
+                            txtInfo.AppendText(cadena);
+                            txtInfo.AppendText(Environment.NewLine);
+                        }
+                        else
+                        {
+                            acabado = true; ;
+                        }
+                    }
+                }
+                catch (IOException)
+                {
+                    txtInfo.AppendText("Sin conexion con el servidor");
+                }
+                acabado = false;
+                conectar();
+                try
+                {
+                    sw.WriteLine("list");
+                    sw.Flush();
+                    while (!acabado)
+                    {
+                        string cadena = sr.ReadLine();
+                        if (cadena != null)
+                        {
+                            txtInfo.AppendText(cadena);
+                            txtInfo.AppendText(Environment.NewLine);
+                        }
+                        else
+                        {
+                            acabado = true; ;
+                        }
+                    }
+                }
+                catch (IOException)
+                {
+                    txtInfo.AppendText("Sin conexion con el servidor");
+                }
+            }
+        }
+
+
+        private void btCambiarDni_Click(object sender, EventArgs e)
+        {
+            valido = false;
+            txtInfo.Clear();
+            try
+            {
+                string letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+                string dniAValidar = txtDni.Text;
+                if (dniAValidar.Length == 9)
+                {
+                    dniAValidar = dniAValidar.ToUpper();
+                    int num = Convert.ToInt32(dniAValidar.Substring(0, 8));
+                    int resto = num % 23;
+                    if (letras[resto] == dniAValidar[8])
+                    {
+                        dni = dniAValidar;
+                        valido = true;
+                    }
+                    else
+                    {
+                        txtInfo.AppendText("DNI Invalido");
+                    }
+                }
+                else
+                {
+                    txtInfo.AppendText("DNI Invalido");
+                }
+            }
+            catch
+            {
+                txtInfo.AppendText("DNI Invalido");
+                txtDni.Text = dni;
+            }
+            try
+            {
+                IPAddress.Parse(txtIP.Text);
+                Convert.ToInt32(txtPuerto.Text);
+                IP_SERVER = txtIP.Text;
+                port = Convert.ToInt32(txtPuerto.Text);
+            }
+            catch
+            {
+                valido = false;
+                txtIP.Text = IP_SERVER;
+                txtPuerto.Text = port + "";
+                txtInfo.AppendText(Environment.NewLine);
+                txtInfo.AppendText("Error en IP o Puerto");
+            }
+            if (valido)
+            {
+                txtInfo.AppendText("Datos modificados");
+                datos = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(directory))
+            {
+                using (BinaryReader reader = new BinaryReader(new FileStream(directory, FileMode.Open)))
+                {
+                    try
+                    {
+                        dni = reader.ReadString();
+                        IP_SERVER = reader.ReadString();
+                        port = reader.ReadInt32();
+                        txtDni.Text = dni;
+                        txtIP.Text = IP_SERVER;
+                        txtPuerto.Text = port + "";
+                        datos = true;
+                    }
+                    catch { }
+                }
+            }
+        }
+
+        private void btGuardar_Click(object sender, EventArgs e)
+        {
+            btCambiarDni.PerformClick();
+            txtInfo.Clear();
+            if (valido)
+            {
+                using (BinaryWriter writer = new BinaryWriter(new FileStream(directory, FileMode.Create)))
+                {
+                    try
+                    {
+                        writer.Write(dni);
+                        writer.Write(IP_SERVER);
+                        writer.Write(port);
+                        txtInfo.AppendText("Datos Guardados");
+                    }
+                    catch { }
+                }
+            }
+            else
+            {
+                txtInfo.AppendText("Datos Invalidos");
+            }
         }
     }
 }
